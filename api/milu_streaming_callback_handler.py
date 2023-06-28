7dc5afec29e301514f46c5cbe576002e382110fa
Neukiru <malkhazsuladze93@gmail.com>
2023-06-28 17:10:06 +0400

    move to async

--- api/milu_streaming_callback_handler.py
+++ api/milu_streaming_callback_handler.py
@@ -9,14 +9,14 @@ class MiluStreamingCallbackHandler(StreamingStdOutCallbackHandler):
         super().__init__()
         self.sio = sio
     
-    def on_llm_new_token(self, token: str, **kwargs: Any) -> None:
+    async def on_llm_new_token(self, token: str, **kwargs: Any) -> None:
         """Run on new LLM token. Only available when streaming is enabled."""
-        self.sio.emit("on_llm_new_token",{"token_sequence":token})
+        await self.sio.emit("on_llm_new_token",{"token_sequence":token})
 
-    def on_llm_end(self, response: LLMResult, **kwargs: Any) -> None:
+    async def on_llm_end(self, response: LLMResult, **kwargs: Any) -> None:
         """Run when LLM ends running."""
-        self.sio.emit("on_llm_new_token",{"token_sequence_end":True})
+        await self.sio.emit("on_llm_new_token",{"token_sequence_end":True})
 
-    def on_llm_start(self, response: LLMResult, **kwargs: Any) -> None:
+    async def on_llm_start(self, response: LLMResult, **kwargs: Any) -> None:
         """Run when LLM ends running."""
-        self.sio.emit("on_llm_new_token",{"token_sequence_start":True})
+        await self.sio.emit("on_llm_new_token",{"token_sequence_start":True})
