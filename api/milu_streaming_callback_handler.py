cfee0ec5b84b25e30138770b708848fe923ef138
Neukiru <malkhazsuladze93@gmail.com>
2023-06-30 18:37:09 +0400

    refactor

--- api/milu_streaming_callback_handler.py
+++ api/milu_streaming_callback_handler.py
@@ -10,32 +10,28 @@ from uuid import UUID
 
 class MiluStreamingCallbackHandler(AsyncCallbackHandler):
 
-    def __init__(self,sio,**kwargs: Any) -> None:
+    def __init__(self, sio, **kwargs: Any) -> None:
         super().__init__()
         self.sio = sio
         self.user_sid = kwargs.get('user_sid')
         print('milu is connected!')
-    
 
     async def on_chat_model_start(
-    self,
-    serialized: Dict[str, Any],
-    messages: List[List[BaseMessage]],
-    *,
-    run_id: UUID,
-    parent_run_id: Optional[UUID] = None,
-    tags: Optional[List[str]] = None,
-    **kwargs: Any,
+        self,
+        serialized: Dict[str, Any],
+        messages: List[List[BaseMessage]],
+        *,
+        run_id: UUID,
+        parent_run_id: Optional[UUID] = None,
+        tags: Optional[List[str]] = None,
+        **kwargs: Any,
     ) -> Any:
         pass
 
-
     async def on_llm_new_token(self, token: str, **kwargs: Any) -> None:
         """Run on new LLM token. Only available when streaming is enabled."""
-        await self.sio.emit("assistant_response",{'message': token, 'message_end': False},to=self.user_sid)
-
-        
+        await self.sio.emit("assistant_response", {'message': token, 'message_end': False}, to=self.user_sid)
 
     async def on_llm_end(self, response: LLMResult, **kwargs: Any) -> None:
         """Run when LLM ends running."""
-        await self.sio.emit("assistant_response",{'message': '', 'message_end': True},to=self.user_sid)
+        await self.sio.emit("assistant_response", {'message': '', 'message_end': True}, to=self.user_sid)
